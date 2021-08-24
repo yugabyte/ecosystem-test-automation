99dc995a94b3cdefb21d20340562782eae4ffbaa
Amogh Shetkar <ashetkar@yugabyte.com>
2021-08-24 18:06:24 +0530

    Return correct exit code from top-level script

--- start.sh
+++ start.sh
@@ -8,8 +8,13 @@ pushd $CURRENT_DIR_PATH
 
 # Sequelize
 YBDB_IMAGE=latest bash ./sequelize/start.sh
+SEQU_RUN=$?
 
 # gORM
 YBDB_IMAGE=latest bash ./gorm/start.sh
+GORM_RUN=$?
 
 popd
+
+EXIT_CODE=`expr $SEQU_RUN + $GORM_RUN`
+exit $EXIT_CODE
