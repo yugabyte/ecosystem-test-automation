bc4480b8e2079dfca77633b2ea3f6c31e74fcb95
Amogh Shetkar <ashetkar@yugabyte.com>
2021-08-24 19:40:48 +0530

    Minor change

--- gorm/run-app.sh
+++ gorm/run-app.sh
@@ -26,8 +26,8 @@ REST_PID1=`echo $!`
 sleep 10
 
 REST_PID=`pgrep -P $REST_PID1`
-ps -ef | grep $REST_PID1
 printf "REST server setup done. PIDs: $REST_PID1, $REST_PID\n"
+ps -ef | grep $REST_PID1
 echo "kill -9 $REST_PID1" >> $CURRENT_DIR_PATH/process-cleanup.sh
 echo "kill -9 $REST_PID" >> $CURRENT_DIR_PATH/process-cleanup.sh
 
