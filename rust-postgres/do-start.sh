9344273de1d722ff7f3e4815f7f449f9e94d3de5
Harsh Daryani <82017686+HarshDaryani896@users.noreply.github.com>
2023-12-05 13:54:13 +0530

    Minor spelling change

--- rust-postgres/do-start.sh
+++ rust-postgres/do-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-prinf "Installing cargo ...\n"
+printf "Installing cargo ...\n"
 curl https://sh.rustup.rs -sSf > cargo_install.sh
 chmod +x cargo_install.sh
 ./cargo_install.sh -y
 source "$HOME/.cargo/env" 
-prinf "cargo Installed ...\n"
+printf "cargo Installed ...\n"
 
 # Start the test/example application and generate reports
 printf "Executing run-app.sh ...\n"
