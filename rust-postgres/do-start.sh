522e51ed1e03e1c2c2bb4b586b2c18e70dbb2cce
Harsh Daryani <82017686+HarshDaryani896@users.noreply.github.com>
2023-12-05 14:28:15 +0530

    Update do-start.sh to install cargo (#8)
    * Update do-start.sh to install cargo for rust-postgres driver examples.

--- rust-postgres/do-start.sh
+++ rust-postgres/do-start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+printf "Installing cargo ...\n"
+curl https://sh.rustup.rs -sSf > cargo_install.sh
+chmod +x cargo_install.sh
+./cargo_install.sh -y
+source "$HOME/.cargo/env" 
+printf "cargo Installed ...\n"
+
 # Start the test/example application and generate reports
 printf "Executing run-app.sh ...\n"
 ./run-app.sh
