e101407499e535a3d243e7e39f236a0b2e6dd693
Harsh Daryani <82017686+HarshDaryani896@users.noreply.github.com>
2023-12-06 15:27:17 +0530

    Reverting gocql change

--- gocql/run-app.sh
+++ gocql/run-app.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
-DIR="driver-examples"
-if [ -d "$DIR" ]; then
- echo "driver-examples repository already clonned"
- cd driver-examples
- git checkout master
- git pull
-else
- echo "Cloning the driver examples repository"
- git clone git@github.com:yugabyte/driver-examples.git
- cd driver-examples
-fi
-
+echo "Cloning the gocql repository"
+git clone git@github.com:yugabyte/gocql.git
 cd gocql
 
 echo "Running tests"
