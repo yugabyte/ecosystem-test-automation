db9e093b80238ffd728cc3cc25514640a7fcc399
Harsh Daryani <82017686+HarshDaryani896@users.noreply.github.com>
2023-12-06 15:24:08 +0530

    Update run-app.sh node-postgres

--- node-postgres/run-app.sh
+++ node-postgres/run-app.sh
@@ -9,9 +9,19 @@ echo "Installing node-postgres smart driver pool package"
 
 npm install @yugabytedb/pg-pool
 
-echo "Cloning the driver examples repository"
+DIR="driver-examples"
+if [ -d "$DIR" ]; then
+ echo "driver-examples repository already clonned"
+ cd driver-examples
+ git checkout master
+ git pull
+else
+ echo "Cloning the driver examples repository"
+ git clone git@github.com:yugabyte/driver-examples.git
+ cd driver-examples
+fi
 
-git clone git@github.com:yugabyte/driver-examples.git && cd driver-examples/nodejs
+cd nodejs
 npm install
 
 echo "Exporting environment variable YB_PATH with the value of the path of the YugabyteDB installation directory."
