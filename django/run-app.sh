e820447e6ea85c224ed1ebd3d695ca7086e44359
Sfurti-yb <sfurtisarah@gmail.com>
2025-05-14 11:01:06 +0530

    upgrade pip

--- django/run-app.sh
+++ django/run-app.sh
@@ -22,7 +22,7 @@ mkdir -p $DJANGO_TESTS_DIR
 git clone git@github.com:yugabyte/yb-django.git
 
 cd yb-django
-
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 pip3 install -e .
 cd ..
@@ -31,7 +31,7 @@ git clone https://github.com/django/django.git --branch $DJANGO_BRANCH $DJANGO_T
 
 
 cd $DJANGO_TESTS_DIR/django 
-pip3 install . 
+pip3 install -e . 
 pip3 install -r tests/requirements/py3.txt; cd ../../
 
 create_settings() {
