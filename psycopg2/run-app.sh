a4138dae74eade763d8504711e926867291fff36
Harsh Daryani <harsh.daryani.6211@gmail.com>
2025-08-17 18:59:11 +0000

    Try out fix

--- psycopg2/run-app.sh
+++ psycopg2/run-app.sh
@@ -27,12 +27,13 @@ run_test() {
 if [ -d "$DIR" ]; then
     echo "driver-examples repository is already present"
     cd driver-examples
-    git checkout main
+    git checkout psycopg2-fix
     git pull
 else
     echo "Cloning the driver examples repository"
     git clone git@github.com:yugabyte/driver-examples.git
     cd driver-examples
+    git checkout psycopg2-fix
 fi
 
 cd python-psycopg2/
