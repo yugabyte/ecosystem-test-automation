663d2ac87949b557d474d00aae6b1cdafeff3d94
Harsh Daryani <harsh.daryani.6211@gmail.com>
2025-11-20 20:22:58 +0000

    Command correction

--- pgx/start-ybdb.sh
+++ pgx/start-ybdb.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Start YugabyteDB
-$YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --advertise_addresses=127.0.0.1
+$YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --advertise_address=127.0.0.1
