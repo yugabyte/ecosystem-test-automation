c031b68dedf97df658b0807886a9e2b31a30cd9f
Harsh Daryani <harsh.daryani.6211@gmail.com>
2025-12-17 10:56:09 +0000

    Use pgx master branch

--- pgx/run-app.sh
+++ pgx/run-app.sh
@@ -88,13 +88,12 @@ echo "Executing start-ybdb.sh ...\n"
 if [ -d "$DIR2" ]; then
  echo "pgx repository is already present"
  cd pgx
- git checkout falky-tests
+ git checkout master
  git pull
 else
  echo "Cloning the pgx repository"
  git clone git@github.com:yugabyte/pgx.git
  cd pgx
- git checkout falky-tests
 fi
 
 echo "Running upstream tests"
