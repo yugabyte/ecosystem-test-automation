a2aa8e205583de771641cbfacb9e0180cb9a0f7d
Amogh Shetkar <ashetkar@yugabyte.com>
2025-12-17 12:15:40 +0530

    Add an echo message

--- django/start-ybdb.sh
+++ django/start-ybdb.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Start YugabyteDB
+echo "Starting YugabyteDB with connection manager ..."
 $YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --tserver_flags "enable_ysql_conn_mgr=true" --ui false
