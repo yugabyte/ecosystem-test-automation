a91c9e69e5931fd27511f408ff63df28c379539d
Amogh Shetkar <ashetkar@yugabyte.com>
2025-12-17 12:26:15 +0530

    set advertise_address to 127.0.0.1

--- django/start-ybdb.sh
+++ django/start-ybdb.sh
@@ -3,4 +3,4 @@ set -e
 
 # Start YugabyteDB
 echo "Starting YugabyteDB with connection manager ..."
-$YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --tserver_flags "enable_ysql_conn_mgr=true" --ui false
+$YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --advertise_address=127.0.0.1 --tserver_flags "enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr" --ui false
