4ad4ba354576e021e9bdf5d8f02aac43022a40cf
Amogh Shetkar <ashetkar@yugabyte.com>
2025-12-20 06:46:04 +0000

    Check python versions

--- gocql/start.sh
+++ gocql/start.sh
@@ -8,6 +8,22 @@ CURRENT_DIR_PATH=`realpath $CURRENT_DIR`
 
 cd $CURRENT_DIR_PATH
 
+printf "Python check starting\n"
+ls -1 /usr/bin/python* | xargs ls -l
+ls -1 /usr/local/bin/python* | xargs ls -l
+
+printf "which pythons\n"
+which python
+which python3
+which python3.8
+which python3.9
+which python3.12
+python --version
+printf "PATH:\n"
+echo $PATH
+
+printf "\nPython check done\n"
+
 # Start the run
 YBDB_IMAGE_PATH=$YBDB_IMAGE_PATH bash ./do-start.sh
 SUCCESS="$?"
