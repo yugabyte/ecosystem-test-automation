84d1b481ca54ed1d6ea3f9246dd7b50064a76c1c
Amogh Shetkar <ashetkar@users.noreply.github.com>
2025-12-29 19:40:29 +0530

    Enable connection manager for django tests (#41)
    * Enable connection manager for django tests

--- django/start-ybdb.sh
+++ django/start-ybdb.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Start YugabyteDB
-$YUGABYTE_HOME_DIRECTORY/bin/yb-ctl create
+echo "Starting YugabyteDB with connection manager ..."
+$YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --advertise_address=127.0.0.1 --tserver_flags "enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr" --ui false
