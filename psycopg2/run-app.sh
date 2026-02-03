cd0d8dbd2320ee54bb821789ab85c0b21fae4fe0
Amogh Shetkar <ashetkar@yugabyte.com>
2026-02-03 13:21:54 +0000

    checkout debug branch for testing

--- psycopg2/run-app.sh
+++ psycopg2/run-app.sh
@@ -27,12 +27,14 @@ run_test() {
 if [ -d "$DIR" ]; then
     echo "driver-examples repository is already present"
     cd driver-examples
-    git checkout main
+    git checkout python-test-fixes
     git pull
 else
     echo "Cloning the driver examples repository"
     git clone git@github.com:yugabyte/driver-examples.git
     cd driver-examples
+    git checkout python-test-fixes
+    git pull
 fi
 
 cd python-psycopg2/
