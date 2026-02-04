7c0ff6f6a0be8c50cd27b0df632ffb8631bf78e1
Amogh Shetkar <ashetkar@yugabyte.com>
2026-02-04 12:44:52 +0000

    include a missing testcase

--- pgx/run-app.sh
+++ pgx/run-app.sh
@@ -42,12 +42,14 @@ run_test() {
 if [ -d "$DIR1" ]; then
  echo "driver-examples repository is already present"
  cd driver-examples
- git checkout main
+ git checkout python-test-fixes
  git pull
 else
  echo "Cloning the driver examples repository"
  git clone git@github.com:yugabyte/driver-examples.git
  cd driver-examples
+ git checkout python-test-fixes
+ git pull
 fi
 
 cd go/pgx
@@ -78,6 +80,8 @@ run_test "fallbackTest" "checkNodeDownPrimary" "End of checkNodeDownPrimary() ..
 run_test "rr" "clusterAwareRRTest" "Closing the application ..." "pgx/start.sh"
 
 run_test "rr" "topologyAwareRRTest" "Closing the application ..." "pgx/start.sh"
+
+run_test " " "multipool" "Closing the application ..." "pgx/start.sh"
 
 cd ../../..
 
