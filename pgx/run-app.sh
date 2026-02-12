016d8fda4daa272cbe81226e216127a79b33541e
Amogh Shetkar <ashetkar@yugabyte.com>
2026-02-12 18:51:06 +0530

    use correct path for test report

--- pgx/run-app.sh
+++ pgx/run-app.sh
@@ -90,12 +90,12 @@ run_test "rr" "topologyAwareRRTest" "Closing the application ..." "pgx/start.sh"
 run_test "multipool" "multipool" "Closing the multi-pool application ..." "pgx/start.sh"
 
 # Finalize the JSON report
-sed -i '$ s/,$//' ../temp_report.json # Remove trailing comma from the last JSON object
-echo "]" >> ../temp_report.json
-sed -i 's/\t/    /g' ../temp_report.json # Replace tabs with spaces
+sed -i '$ s/,$//' ../../../temp_report.json # Remove trailing comma from the last JSON object
+echo "]" >> ../../../temp_report.json
+sed -i 's/\t/    /g' ../../../temp_report.json # Replace tabs with spaces
 
 # Move the temporary report to the final report file
-mv ../temp_report.json "$REPORT_FILE"
+mv ../../../temp_report.json "$REPORT_FILE"
 
 # Display the JSON report
 echo "TEST REPORT -------------------------"
