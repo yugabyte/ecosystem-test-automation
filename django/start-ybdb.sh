fd01f194f0ee21c92edc391f8411acfa5d656fe6
Amogh Shetkar <ashetkar@yugabyte.com>
2026-03-17 19:33:06 +0530

    fix quoting of tserver flags

--- django/start-ybdb.sh
+++ django/start-ybdb.sh
@@ -4,7 +4,7 @@ set -e
 # Start YugabyteDB
 if [ $ENABLE_CM == "true" ]; then
   echo "Starting YugabyteDB cluster with Connection Manager..."
-  enable_cm_flag='--tserver_flags "enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr"'
+  enable_cm_flag="--tserver_flags enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr"
 fi
 
 $YUGABYTE_HOME_DIRECTORY/bin/yugabyted start --advertise_address=127.0.0.1 $enable_cm_flag --ui false
--- liquibase/start-ybdb.sh
+++ liquibase/start-ybdb.sh
@@ -6,7 +6,7 @@ $YUGABYTE_HOME_DIRECTORY/bin/yb-ctl destroy
 
 if [ $ENABLE_CM == "true" ]; then
   echo "Starting YugabyteDB cluster with Connection Manager..."
-  enable_cm_flag='--tserver_flags "enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr"'
+  enable_cm_flag="--tserver_flags enable_ysql_conn_mgr=true,allowed_preview_flags_csv=enable_ysql_conn_mgr"
 fi
 
 # Start a new YugabyteDB cluster
