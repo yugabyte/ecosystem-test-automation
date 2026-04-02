4b7050e0c57c404c5d99dd383491985167f0cb23
Harsh Daryani <82017686+HarshDaryani896@users.noreply.github.com>
2026-04-02 08:46:57 +0530

    Change git branch to python-enable-cm in run-app.sh

--- rust-postgres/run-app.sh
+++ rust-postgres/run-app.sh
@@ -26,12 +26,14 @@ run_test() {
 if [ -d "$DIR" ]; then
  echo "driver-examples repository is already present"
  cd driver-examples
- git checkout main
+ git checkout python-enable-cm
  git pull
 else
  echo "Cloning the driver examples repository"
  git clone git@github.com:yugabyte/driver-examples.git
  cd driver-examples
+ git checkout python-enable-cm
+ git pull
 fi
 
 cd rust/rust_ysql
